9a6ad6fd5561f9e772b17e73487a5e734cbf923c
Victor N <vns@teknik.io>
2018-10-23 09:28:59 -0300

    Finished all functions and added to each of them the return command

--- calc.py
+++ calc.py
@@ -1,14 +1,14 @@
 def add(x, y):
-    x + y
+    return x + y
 
 def subtract(x, y):
-    x - y
+    return x - y
 
 def multiply(x, y):
-    x * y
+    return x * y
 
 def divide(x, y):
-    x / y
+    return x / y
 
 def square(x, y):
-    x ** y
+    return x ** y
